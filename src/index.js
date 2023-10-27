9d5078f04a20dedce4a87eefcc9a0c4ec94eda0b
Vishal Sharma <785vishal443@gmail.com>
2023-10-27 22:47:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import counterReducer from './reducers';
+
+const store = createStore(counterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 
 
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('root')
-// );
-
+ReactDOM.render(
+  <Provider store={store}><App /></Provider>,
+  document.getElementById('root')
+);
