87576f3833111b30d095d31856a1032a8b02f221
Vishal Sharma <785vishal443@gmail.com>
2023-10-27 22:45:20 +0530

    Update index.js

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+const counterReducer = (state = {counter: 0}, action) =>{
+    switch(action.type){
+        case 'INCREMENT':
+            return {...state, counter: state.counter + 1};
+        case 'DECREMENT':
+            return {...state, counter: state.counter - 1};
+        default:
+            return state;
+    }
+}
+
+export default counterReducer;
