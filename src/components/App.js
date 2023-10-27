85c90ff8985342e06d8286bdedec71c14c2c94ea
Vishal Sharma <785vishal443@gmail.com>
2023-10-27 22:47:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {increment, decrement} from '../actions/action';
 
 function App() {
-
+  const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch();
   return (
     <div id='main'>
-      
+      <button onClick={() => dispatch(increment())}>+</button>
+      <span data-testid='counter'>{counter}</span>
+      <button onClick={() => dispatch(decrement())}>-</button>
     </div>
   );
 }
